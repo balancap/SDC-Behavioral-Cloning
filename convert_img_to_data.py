7cea039b4b22cdacf73d42c8fcee514413b8d32e
Paul Balanca <paul.balanca@gmail.com>
2016-11-21 17:59:40 +0000

    ADD: HDF5 file format to convert.

--- convert_img_to_data.py
+++ convert_img_to_data.py
@@ -5,6 +5,7 @@ import sys
 
 import pickle
 import csv
+import h5py
 
 import cv2
 import math
@@ -13,9 +14,12 @@ import numpy as np
 import matplotlib.image as mpimg
 
 IMG_SHAPE = (160, 320, 3)
-SUBSAMPLING = 2
+SUBSAMPLING = 5
 
 
+# ============================================================================
+# Load / Save data: old way!
+# ============================================================================
 def load_data(path):
     """Load DATA from path: images + car information.
 
@@ -29,8 +33,8 @@ def load_data(path):
     list_imgs = os.listdir(path + 'IMG/')
     print('Number of images: %i. Sub-sampling: %i.' % (len(list_imgs),
                                                        SUBSAMPLING))
-
     nb_imgs = math.ceil(len(list_imgs) / float(SUBSAMPLING))
+    # Data structure.
     data = {
                 'images': np.zeros((nb_imgs, *IMG_SHAPE), dtype=np.uint8),
                 'angle': np.zeros((nb_imgs, ), dtype=np.float32),
@@ -78,13 +82,63 @@ def save_np_data(path, data):
     np.savez(filename, **data)
 
 
+# ============================================================================
+# Load / Save data: HDF5 file.
+# ============================================================================
+def create_hdf5(path):
+    """Create HDF5 file from images and CSV data on the disk.
+
+    Return:
+        Dictionary containing the following entries:
+         - images;
+         - angle: steering angle;
+         - throttle;
+         - speed;
+    """
+    list_imgs = os.listdir(path + 'IMG/')
+    print('Number of images: %i. Sub-sampling: %i.' % (len(list_imgs),
+                                                       SUBSAMPLING))
+    nb_imgs = math.ceil(len(list_imgs) / float(SUBSAMPLING))
+
+    with h5py.File(path + 'dataset.hdf5', 'w') as f, open(path + 'driving_log.csv', 'r') as fcsv:
+        # Create HDF5 dataset.
+        images = f.create_dataset('images', (nb_imgs, *IMG_SHAPE), dtype='uint8')
+        angle = f.create_dataset('angle', (nb_imgs, ), dtype='float32')
+        throttle = f.create_dataset('throttle', (nb_imgs, ), dtype='float32')
+        speed = f.create_dataset('speed', (nb_imgs, ), dtype='float32')
+
+        # Load CSV information file.
+        creader = csv.reader(fcsv)
+        csv_list = list(creader)
+
+        # Load image, when exists, and associated data.
+        idx_subsample = 0
+        for a in csv_list:
+            filename = a[0]
+            if os.path.isfile(filename):
+                idx_subsample += 1
+                if idx_subsample % SUBSAMPLING == 0:
+                    j = idx_subsample // SUBSAMPLING
+                    sys.stdout.write('\r>> Converting image %d/%d' % (j+1, nb_imgs))
+                    sys.stdout.flush()
+
+                    # Save data to HDF5.
+                    images[j] = mpimg.imread(filename)
+                    angle[j] = float(a[3])
+                    throttle[j] = float(a[4])
+                    speed[j] = float(a[6])
+        print('')
+
+
 def main():
     path = './data/1/'
 
     # Load data and 'pickle' dump.
-    data = load_data(path)
-    save_np_data(path, data)
+    # data = load_data(path)
+    # save_np_data(path, data)
     # dump_data(path, data)
+
+    create_hdf5(path)
 
 if __name__ == '__main__':
     main()
